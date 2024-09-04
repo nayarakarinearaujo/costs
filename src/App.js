4386aab59983c86a42790663d7604b550c8e38bf
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-04 11:22:12 -0300

    Iniciando projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Footer from "./components/pages/Footer";
 
 function App() {
   return (
-    <div className="App">
-     <p>Costs</p>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
