d1cebcff6ae6ba3f4323e80379b89fb942cfab30
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-06 09:26:41 -0300

    Ajuste no css

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,26 +6,24 @@ import logo from "../../img/costs_logo.png";
 
 function Navbar() {
   return (
-    <div className={styles.navbar}>
-      <nav>
+      <nav className={styles.navbar}>
         <Container>
-          <Link to="/">
-            <img src={logo} alt="Costs"></img>
-          </Link>
-          <ul className={styles.list}>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contato</Link>
-            </li>
-            <li>
-              <Link to="/company">Empresa</Link>
-            </li>
-          </ul>
+        <Link to="/">
+          <img src={logo} alt="Costs"></img>
+        </Link>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/contact">Contato</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/company">Empresa</Link>
+          </li>
+        </ul>
         </Container>
       </nav>
-    </div>
   );
 }
 
