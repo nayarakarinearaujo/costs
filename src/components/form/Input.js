7920359c72fdfd3e17a19cb661d840500150e945
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-11 11:04:45 -0300

    Feito o form

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,13 +1,11 @@
 import React from 'react'
 
-import React from 'react';
 import styles from './Input.module.css'
 
 function Input({ type, text, name, placeholder, handleOnChange, value }) {
     return (
         <div className={styles.form_control}>
-            <label 
-            htmlFor={name}>{text}</label>
+            <label htmlFor={name}>{text}</label>
             <input 
             type={type} 
             name={name} 
--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import Input from '../form/Input'; 
 
-import styles from './ProjectForm.module.css'
+import styles from './ProjectForm.module.css';
 
 function ProjectForm() {
     return (
         <form className={styles.form}>
             <div>
-                <input type='text' placeholder='Insira o nome do projeto' />
+                <Input 
+                    type="text" 
+                    text="Nome do Projeto" 
+                    name="name" 
+                    placeholder="Insira o nome do projeto" 
+                />
             </div>
             <div>
-                <input type='number' placeholder='Insira o orçamento total' />
+                <Input 
+                    type="number" 
+                    text="Orçamento do Projeto" 
+                    name="budget" 
+                    placeholder="Insira o orçamento total" 
+                />
             </div>
             <div>
                 <select name='category_id'>
-                    <option disabled selected >Selecione a categoria</option>
+                    <option disabled selected>Selecione a categoria</option>
                 </select>
             </div>
-
             <div>
-                <input type='submit' value='Criar projeto'></input>
+                <input type='submit' value='Criar projeto' />
             </div>
         </form>
-    )
+    );
 }
 
-export default ProjectForm
+export default ProjectForm;
