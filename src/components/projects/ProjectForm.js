77483fa51cdf303d5feac340d2a6fca649f1c403
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-11 12:11:51 -0300

    Pegando os dados do json

--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
@@ -6,6 +8,23 @@ import styles from './ProjectForm.module.css';
 
 
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'aplication/json'
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err));
+    }, [])
+
     return (
         <form className={styles.form}>
             <div>
@@ -28,6 +47,7 @@ function ProjectForm({btnText}) {
                 <Select
                     name="category_id"
                     text="Selecione a categoria"
+                    options={categories}
                 />
             </div>
             <div>
