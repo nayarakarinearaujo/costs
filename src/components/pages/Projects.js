f79e6ac7cc6cac6f5d3604c4a0b4efeea1754f80
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-26 11:32:04 -0300

    Ajustes

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,21 +2,31 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 import Message from '../layout/Message';
+import Container from '../layout/Container';
+import LinkButton from '../layout/LinkButton';
+
+import styles from './Projects.module.css'
 
 function Projects() {
   const location = useLocation();
   let message = '';
 
-  // Verifica se o estado contém a mensagem
   if (location.state) {
     message = location.state.message;
   }
 
   return (
-    <div>
-      <h1>Meus projetos</h1>
-      {/* Renderiza a mensagem se houver */}
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus projetos</h1>
+        <LinkButton to='/newproject' text='Criar projeto' />
+      </div>
+
       {message && <Message type="success" msg={message} />}
+
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 }
